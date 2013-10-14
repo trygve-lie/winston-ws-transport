c48755114162a3db74b143029b90777aedab1c8b
Trygve Lie <github@trygve-lie.com>
2013-10-14 14:07:39 +0200

    Initial commit of code

---
+++ example/simple.js
@@ -0,0 +1,62 @@
+/* jshint node: true, strict: true */
+
+"use strict";
+
+var winston     = require('winston'),
+    winstonWs   = require('./');
+
+
+
+// Set up logger with a custom debug level
+
+var log = new (winston.Logger)({
+    levels: {
+        debug   : 0,
+        info    : 1,
+        warn    : 2,
+        error   : 3
+    },
+    exitOnError : false,
+    transports  : [
+        new (winston.transports.Console)({
+            silent              : false,
+            level               : 'debug',
+            colorize            : true,
+            handleExceptions    : true
+        }),
+        new (winstonWs.WebSocket)({
+            silent              : false,
+            level               : 'debug',
+            colorize            : true,
+            handleExceptions    : true,
+            levels              : {
+                                    debug   : 0,
+                                    info    : 1,
+                                    warn    : 2,
+                                    error   : 3
+                                },
+            colors              : {
+                                    debug   : 'blue',
+                                    info    : 'green',
+                                    warn    : 'yellow',
+                                    error   : 'red'
+                                }
+        })
+    ]
+});
+
+winston.addColors({
+    debug   : 'blue',
+    info    : 'green',
+    warn    : 'yellow',
+    error   : 'red'
+});
+
+
+
+
+// Schedule some logging
+log.info('hello');
+setInterval(function(){
+    log.info('The time is: ' + new Date());
+}, 1000);
